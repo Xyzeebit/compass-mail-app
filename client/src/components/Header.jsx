02f3ba8670e082a0d8e680f29221218be63ea7b5
Donald Dominic <gotodonald@gmail.com>
2022-07-24 11:54:07 +0100

    added header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,49 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/header.css';
-import logo from '../mailbox-logo.png';
-import { IoSearch } from 'react-icons/io5';
-import { RiEqualizerFill } from 'react-icons/ri';
+import "../styles/header.css";
+import logo from "../mailbox-logo.png";
+import { IoSearch } from "react-icons/io5";
+import { RiEqualizerFill } from "react-icons/ri";
 
 export default function Header({ dispatch }) {
-    const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
 
-    const handleSearch = (evt) => {
-        evt.preventDefault();
-    }
-    return (
-        <header>
-            <button className='toggle-sidebar'
-                onClick={() => dispatch({ type: 'TOGGLE_SIDEBAR' })}
-            >
-                <div />
-                <div />
-                <div />
-            </button>
-            <div className='brand' title='Packmail brand name'>
-                <img
-                    src={logo}
-                    alt='Packmail logo featuring a red envelope'
-                    width="50"
-                    height="50"
-                />
-                <h1>PACKMAIL</h1>
-            </div>
+  const handleSearch = (evt) => {
+    evt.preventDefault();
+    alert(searchInput);
+    setSearchInput("");
+  };
+  return (
+    <header>
+      <button
+        className="toggle-sidebar"
+        onClick={() => dispatch({ type: "TOGGLE_SIDEBAR" })}
+      >
+        <div />
+        <div />
+        <div />
+      </button>
+      <div className="brand" title="Packmail brand name">
+        <img
+          src={logo}
+          alt="Packmail logo featuring a red envelope"
+          width="50"
+          height="50"
+        />
+        <h1>PACKMAIL</h1>
+      </div>
 
-            <form onSubmit={handleSearch}>
-                <div>
-                    <IoSearch size={25} />
-                </div>
-                <input
-                    type='text'
-                    value={searchInput}
-                    onChange={({ target }) => setSearchInput(target.value)}
-                    placeholder="Search mail..."
-                />
-                <button>
-                    <RiEqualizerFill size={25} />
-                </button>
-            </form>
-        </header>
-    );
+      <div className="search-box">
+        <form onSubmit={handleSearch}>
+          <div>
+            <IoSearch color="#666" size={25} />
+          </div>
+          <input
+            type="text"
+            value={searchInput}
+            onChange={({ target }) => setSearchInput(target.value)}
+            placeholder="Search mail..."
+          />
+          <button>
+            <RiEqualizerFill color="#666" size={20} />
+          </button>
+        </form>
+        <div className="suggestion-box">
+
+        </div>
+      </div>
+    </header>
+  );
 }
