df62f260d7c28ce6f56a4c0da282a1ab5c5c8416
Donald Dominic <gotodonald@gmail.com>
2022-08-03 17:09:44 +0100

    added jwt to signup

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,12 +1,36 @@
 const User = require('../model/userSchema');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 async function signUp(root, args) {
-    const { firstName, lastName, username, password } = args.input;
-    const exist = await userExit(username);
-    if (!exist) {
-        const user = new User({ firstName, lastName, username });
-        user.setPassword(password);
-        await user.save();
+    const signUpPayload = {};
+    try {
+        const { firstName, lastName, username, password } = args.input;
+        const exist = await userExit(username);
+        if (!exist) {
+            const user = new User({ firstName, lastName, username });
+            user.setPassword(password);
+            await user.save();
+            const secret = fs.readFileSync('./secret.txt');
+            const payload = {
+                id: user._id,
+                username,
+
+                iss: 'compass',
+                iat: Date.now(),
+                exp: '24h'
+            }
+            const token = await jwt.sign(payload, secret, { algorithm: 'RS256' });
+            signUpPayload.token = token;
+            signUpPayload.successful = true;
+            return signUpPayload;
+        }
+    } catch (error) {
+        signUpPayload.error = {
+            message: error.message
+        }
+        return signUpPayload;
+
     }
 }
 
