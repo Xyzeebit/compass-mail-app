fcac38d939af065b8bfc208bffad105cf392e64f
Donald Dominic <gotodonald@gmail.com>
2022-08-05 18:55:45 +0100

    made changes to landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { Link } from 'react-router-dom';
 
 import logo from '../images/logo.png'
@@ -7,6 +7,15 @@ import bg from '../images/sustainable.jpg'
 import '../styles/home.css';
 
 export default function Home() {
+
+  const buttonSignUp = useRef(null);
+  const buttonSignIn = useRef(null);
+
+  useEffect(() => {
+    buttonSignUp.current.classList.add('slide-btn');
+    buttonSignIn.current.classList.add('slide-btn');
+
+  }, [])
 
     return (
       <div className="home">
@@ -25,8 +34,8 @@ export default function Home() {
             <h1>Fast, Reliable, Easy, Compass Mailer</h1>
             <p>The top mail client in  the world</p>
             <div className="home-buttons">
-              <Link to="/auth/signup">Sign up</Link>
-              <Link to="/auth/signin">Sign in</Link>
+              <Link to="/auth/signup" ref={buttonSignUp}>Sign up</Link>
+              <Link to="/auth/signin" ref={buttonSignIn}>Sign in</Link>
             </div>
           </div>
         </div>
