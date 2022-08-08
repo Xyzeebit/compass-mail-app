26d74306fc949af880c0542ee6b9214f3b92428f
Donald Dominic <gotodonald@gmail.com>
2022-08-08 11:49:59 +0100

    adding styling to form

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useLocation, Link } from "react-router-dom";
+import { IoEye, IoEyeOff } from 'react-icons/io5';
 
 import '../styles/auth.css';
 
@@ -14,34 +15,59 @@ export default function Auth() {
   const [usernameError, setUsernameError] = useState('');
 
   const [password, setPassword] = useState('');
+  const [passwordOrText, setPasswordOrText] = useState('password');
   const [passwordError, setPasswordError] = useState('');
 
   const [cpassword, setCPassword] = useState('');
+  const [cpasswordOrText, setCPasswordOrText] = useState("password");
   const [cpasswordError, setCPasswordError] = useState('');
 
   const [rememberMe, setRememberMe] = useState(false);
+
+  const passwordRef = useRef(null);
+  const cpasswordRef = useRef(null);
 
     const location = useLocation();
     const paths = location.pathname.split('/')
     const formType = paths[paths.length - 1]
 
-    console.log(paths)
-
     const handleName = (evt) => {
         // setName(evt.target.value);
     }
+
+  const handleFirstName = (evt) => {
+    setFirstName(evt.target.value);
+  }
+  const handleLastName = evt => {
+    setLastName(evt.target.value);
+  }
     const handleUsername = evt => {
         setUsername(evt.target.value);
     }
     const handlePassword = evt => {
         setPassword(evt.target.value)
-    }
+  }
+  const handlePasswordFocused = evt => {
+    passwordRef.current.style.border = '2px solid black';
+  }
+  const handlePasswordBlurred = evt => {
+    passwordRef.current.style.border = "1px solid grey";
+  }
     const handleCPassword = evt => {
         setCPassword(evt.target.value)
     }
+
+  const handleCPasswordFocused = (evt) => {
+    cpasswordRef.current.style.border = "2px solid black";
+  };
+  const handleCPasswordBlurred = (evt) => {
+    cpasswordRef.current.style.border = "1px solid grey";
+  };
     const handleSubmit = evt => {
         evt.preventDefault();
     }
+
+  
 
     return (
       <div className="auth">
@@ -54,7 +80,7 @@ export default function Auth() {
                   type="text"
                   id="first-name"
                   value={firstName}
-                  onChange={handleName}
+                  onChange={handleFirstName}
                 />
               </div>
               <div className="stack">
@@ -63,7 +89,7 @@ export default function Auth() {
                   type="text"
                   id="name"
                   value={lastName}
-                  onChange={handleName}
+                  onChange={handleLastName}
                 />
               </div>
             </>
@@ -81,33 +107,53 @@ export default function Auth() {
 
           <div className="stack">
             <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePassword}
-            />
+            <div className="input-group" ref={passwordRef}>
+              <input
+                type={passwordOrText}
+                id="password"
+                value={password}
+                onChange={handlePassword}
+                onFocus={handlePasswordFocused}
+                onBlur={handlePasswordBlurred}
+              />
+              {passwordOrText === "text" ? (
+                <IoEyeOff onClick={() => setPasswordOrText("password")} />
+              ) : (
+                <IoEye onClick={() => setPasswordOrText("text")} />
+              )}
+            </div>
           </div>
 
           {formType === "signup" ? (
             <div className="stack">
               <label htmlFor="cpassword">Confirm password</label>
-              <input
-                type="password"
-                id="cpassword"
-                value={cpassword}
-                onChange={handleCPassword}
-              />
+              <div className="input-group" ref={cpasswordRef}>
+                <input
+                  type={cpasswordOrText}
+                  id="cpassword"
+                  value={cpassword}
+                  onChange={handleCPassword}
+                  onFocus={handleCPasswordFocused}
+                  onBlur={handleCPasswordBlurred}
+                />
+                {cpasswordOrText === "text" ? (
+                  <IoEyeOff onClick={() => setCPasswordOrText("password")} />
+                ) : (
+                  <IoEye onClick={() => setCPasswordOrText("text")} />
+                )}
+              </div>
             </div>
           ) : (
             <div className="stack">
               <input
-                  type="checkbox"
-                  id="remember-me"
+                type="checkbox"
+                id="remember-me"
                 value={rememberMe}
                 onChange={() => setRememberMe(!rememberMe)}
               />
-              <label htmlFor="remember-me">Remember me</label>
+              <label htmlFor="remember-me" className="remember-me">
+                Remember me
+              </label>
             </div>
           )}
           <div className="stack">
@@ -123,7 +169,7 @@ export default function Auth() {
             </p>
           ) : (
             <p>
-              Already have an account <Link to="/auth/signin">Sign in</Link>
+              Already have an account <Link to="/auth/signin">sign in</Link>
             </p>
           )}
         </form>
