ec78880bee364e9a994ff9b34705f5d9c4394cf8
Donald Dominic <gotodonald@gmail.com>
2022-08-18 09:43:20 +0100

    added drafts and spam controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -117,9 +117,94 @@ async function outbox(args) {
     }
 }
 
+function drafts(args) {
+    const payload = {};
+    const LIMIT = 20;
+
+    try {
+        const { username, page } = args;
+        const box = await MailBox.find({ from: username })
+          .where("trash")
+          .equals(false)
+          .where("draft")
+          .equals(true)
+          .where("spam")
+          .equals(false)
+          .select("_id from to subject body time read")
+          .limit(LIMIT)
+          .skip(LIMIT * page)
+          .sort({ time: "asc" });
+        if (box) {
+          payload.success = true;
+          payload.messages = box.map((i) => {
+            return {
+              id: i._id,
+              from: i.from,
+              to: i.to,
+              subject: i.subject,
+              body: i.body,
+              read: i.read,
+              time: i.time,
+            };
+          });
+        }
+    } catch (error) {
+        payload.success = false;
+        payload.error = {
+          name: "RequestError",
+          message: "request error",
+        };
+    } finally {
+        return payload;
+    }
+}
+
+function spam(args) {
+    const payload = {};
+    const LIMIT = 20;
+    try { 
+        const { username, page } = args;
+        const box = await MailBox.find({ to: username })
+          .where("trash")
+          .equals(false)
+          .where("draft")
+          .equals(false)
+          .where("spam")
+          .equals(true)
+          .select("_id from to subject body time read")
+          .limit(LIMIT)
+          .skip(LIMIT * page)
+          .sort({ time: "asc" });
+        if (box) {
+          payload.success = true;
+          payload.messages = box.map((i) => {
+            return {
+              id: i._id,
+              from: i.from,
+              to: i.to,
+              subject: i.subject,
+              body: i.body,
+              read: i.read,
+              time: i.time,
+            };
+          });
+        }
+    } catch (error) {
+        payload.success = false;
+        payload.error = {
+          name: "RequestError",
+          message: "request error",
+        };
+    } finally {
+        return payload;
+    }
+}
+
 module.exports = {
     getUser,
     inbox,
-    outbox
+    outbox,
+    drafts,
+    spam
 }
 
