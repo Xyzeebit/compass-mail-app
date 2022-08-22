0b4549e960e9a14672f3005838a7ebc796eaab4e
Donald Dominic <gotodonald@gmail.com>
2022-08-22 12:07:07 +0100

    added add contact form

--- client/src/components/Contact-v2.jsx
+++ client/src/components/Contact-v2.jsx
@@ -2,36 +2,143 @@ import { useState } from "react";
 import { IoPerson } from "react-icons/io5";
 
 export default function Contacts({ contacts, dispatch }) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [formVisible, setFormVisible] = useState(false);
+
+    function addContact() {
+
+    }
+    function cancel() {
+        setName('');
+        setEmail('');
+        setFormVisible(false);
+    }
+    
     return (
-        <aside className={`contacts`}>
-            <div className={`profile`}>
-                <p className="text">Profile</p>
-                <span className="avatar photo">
-                    <IoPerson size={40} />
-                </span>
-                <div className="name">
-                    <p>John Doe</p>
-                    <button>Add user</button>
-                </div>
+      <aside className={`contacts`}>
+        <div className={`profile`}>
+          <p className="text">Profile</p>
+          <span className="avatar photo">
+            <IoPerson size={40} />
+          </span>
+          <div className="name">
+            <p>John Doe</p>
+            <button onClick={() => setFormVisible(true)}>Add contact</button>
+          </div>
             </div>
-            <ul className="contact-list">
-                <li className="contact">
-                    <span className="avatar">
-                        <IoPerson size={30} />
-                    </span>
-                    <div>
-                        <h3>John Smith</h3>
-                        <p>johnsmith@compass.com</p>
-                    </div>
-                </li>
-                <li>User 1</li>
-                <li>User 2</li>
-                <li>User 3</li>
-                <li>User 4</li>
-                <li>User 5</li>
-            </ul>
-        </aside>
-    )
+            <div>
+                <h4>Add new contact</h4>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(evt) => setName(evt.target.value)}
+                />
+            </div>
+        <ul className="contact-list">
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+          <li className="contact">
+            <span className="avatar">
+              <IoPerson size={30} />
+            </span>
+            <div>
+              <h3>John Smith</h3>
+              <p>johnsmith@compass.com</p>
+            </div>
+          </li>
+        </ul>
+      </aside>
+    );
 }
 
 // https://random-data-api.com/api/v2/users?size=2
