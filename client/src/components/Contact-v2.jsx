b5b68e944fe777a59e2fe9f684834c600d73ff13
Donald Dominic <gotodonald@gmail.com>
2022-08-22 23:03:32 +0100

    added loading skeleton to contacts component

--- client/src/components/Contact-v2.jsx
+++ client/src/components/Contact-v2.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoPerson } from "react-icons/io5";
 
 export default function Contacts({ contacts, dispatch }) {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [formVisible, setFormVisible] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [list, setList] = useState([]);
 
     function addContact() {
 
@@ -13,11 +15,29 @@ export default function Contacts({ contacts, dispatch }) {
         setName('');
         setEmail('');
         setFormVisible(false);
+  }
+  
+  useEffect(() => {
+    async function getMockUsers() {
+      // https://random-data-api.com/api/v2/users?size=2
+      const resp = await fetch(`https://random-data-api.com/api/v2/users?size=${10}`);
+      const users = await resp.json();
+      if (resp.ok && users) {
+        setList(users);
+      }
     }
+  })
     
-    return (
-      <aside className={`contacts`}>
-        <h1 className="app-name">compass</h1>
+  return (
+    <aside className={`contacts`}>
+      <h1 className="app-name">compass</h1>
+      {loading ? (
+        <div className="card p-5 p-skeleton">
+          <div className="p-text" />
+          <div className="p-all" />
+          <div className="p-image" />
+        </div>
+      ) : (
         <div className={`profile`}>
           <p className="text">Profile</p>
           <span className="avatar photo">
@@ -28,129 +48,67 @@ export default function Contacts({ contacts, dispatch }) {
             <button onClick={() => setFormVisible(true)}>Add contact</button>
           </div>
         </div>
-        {formVisible && (
-          <div className="contact-form">
-            <h4>Add new contact</h4>
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(evt) => setName(evt.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(evt) => setEmail(evt.target.value)}
-            />
-            <button className="form-button save" onClick={addContact}>Save</button>
-            <button className="form-button cancel" onClick={cancel}>Cancel</button>
-          </div>
+      )}
+      {formVisible && (
+        <div className="contact-form">
+          <h4>Add new contact</h4>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(evt) => setName(evt.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(evt) => setEmail(evt.target.value)}
+          />
+          <button className="form-button save" onClick={addContact}>
+            Save
+          </button>
+          <button className="form-button cancel" onClick={cancel}>
+            Cancel
+          </button>
+        </div>
+      )}
+
+      <ul className="contact-list">
+        {list.length === 0 && (
+          <>
+            <li className="c-skeleton card">
+              <div className="c-image" />
+              <div className="c-group">
+                <div className="c-name" />
+                <div className="c-email" />
+              </div>
+            </li>
+            <li className="c-skeleton card">
+              <div className="c-image" />
+              <div className="c-group">
+                <div className="c-name" />
+                <div className="c-email" />
+              </div>
+            </li>
+          </>
         )}
-        
-        <ul className="contact-list">
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-          <li className="contact">
-            <span className="avatar">
-              <IoPerson size={30} />
-            </span>
-            <div>
-              <h3>John Smith</h3>
-              <p>johnsmith@compass.com</p>
-            </div>
-          </li>
-        </ul>
-      </aside>
-    );
+        {list &&
+          (<>
+            {list.map((con, i) => (
+            <li key={i} className={`contact`}>
+              <span className="avatar">
+                <IoPerson size={30} />
+              </span>
+              <div>
+                <h3>{con.name}</h3>
+                <p>{con.email}</p>
+              </div>
+            </li>
+            ))}
+          </>)}
+      </ul>
+    </aside>
+  );
 }
 
 // https://random-data-api.com/api/v2/users?size=2
