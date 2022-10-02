b04322fe20c04bcd52d509a0e44db5c1bee335bf
Donald Dominic <gotodonald@gmail.com>
2022-10-02 07:41:18 +0100

    changed empty list component text and image colors

--- client/src/components/EmptyList.jsx
+++ client/src/components/EmptyList.jsx
@@ -3,7 +3,7 @@ import { IoMailOpenSharp } from "react-icons/io5"
 export default function EmptyList({ text }) {
     return (
         <div className="empty-list">
-            <IoMailOpenSharp color="#666" size={200} />
+            <IoMailOpenSharp color="#3940a7e0" size={200} />
             <h1>{text}</h1>
         </div>
     )
