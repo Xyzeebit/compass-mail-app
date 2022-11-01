d11218e5f59a984196557ec3ab326464049928d8
Donald Dominic <gotodonald@gmail.com>
2022-11-01 14:23:45 +0100

    added fetch message gql client queries

--- client/src/pages/Mail.jsx
+++ client/src/pages/Mail.jsx
@@ -1,22 +1,20 @@
 import Layout from '../components/Layout-v2';
 import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { messageQL } from '../queries';
-
+import { useMessage } from '../hooks';
 import combineReducers, { initState } from "../reducer/reducer";
 
 export default function Mail() {
     const [state, dispatch] = useReducer(combineReducers, initState);
     const { sidebar, user } = state;
   const params = useParams();
-  const { loading, error, data } = useQuery(messageQL);
-
+  const data = useMessage(user.username, params.id);
+  console.log(data)
   useEffect(() => {
     document.title = 'Compass | Message'
   }, []);
 
-  
+
   
     return (
       <Layout sidebar={sidebar} user={user} dispatch={dispatch}>
